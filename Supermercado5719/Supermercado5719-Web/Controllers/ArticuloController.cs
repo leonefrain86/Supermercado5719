fe5f54a401f1744809d9d230f78da9e76985fa98
bruno1304 <bruno130402@gmail.com>
2019-10-31 11:18:29 -0300

    act

--- Supermercado5719/Supermercado5719-Web/Controllers/ArticuloController.cs
+++ Supermercado5719/Supermercado5719-Web/Controllers/ArticuloController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Supermercado5719_Biblioteca;
 
 namespace Supermercado5719_Web.Controllers
 {
-    public class ProductoController : Controller
+    public class ArticuloController : Controller
     {
+        private readonly object LiteDbContext db;
+
+        public ArticuloController(LiteDBContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var articulos = db.CollecGetCollections<Articulo>();
         }
     }
 }
