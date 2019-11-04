734c9b522cd3d16231f61aee38d6e44fb2595d77
leonefrain86 <leonefrain86cbo@gmail.com>
2019-11-04 17:45:07 -0300

    actualizado

--- Supermercado5719/Supermercado5719-Web/Controllers/CajaController.cs
+++ Supermercado5719/Supermercado5719-Web/Controllers/CajaController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Supermercado5719_Biblioteca;
 
 namespace Supermercado5719_Web.Controllers
 {
-    public class ArticuloController : Controller
+    public class CajaController : Controller
     {
         public IActionResult Index()
         {
